e24b6104567e4df647d105aca0758dea899b49b5
u6020613113-crypto <u6020613113@gmail.com>
2026-05-09 15:18:43 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -7,7 +7,7 @@ export const botConfig = {
   // =========================
   // `status` options:
   // - "online"    = green dot
-  // - "idle"      = yellow moon
+  // - "idle"      = blue moon
   // - "dnd"       = red do-not-disturb
   // - "invisible" = appears offline
   presence: {
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "skibidi fiutek 🍆🍑💦",
         // Activity type number (0 = Playing).
         type: 0, 
       },
